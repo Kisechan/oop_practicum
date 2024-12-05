02c62d4f0dc3253e80d019274e136ab1b92cd4a6
Kisechan <thekiseplatinyl@outlook.com>
2024-12-05 14:56:39 +0800

    配置Fyne

--- go/web_sql/main.go
+++ go/web_sql/main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"web_sql/route"
 
+	"fyne.io/fyne/v2/app"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func initWeb() {
 	router := gin.Default()
 	route.SetupUserRoutes(router)
 	if err := router.Run(":8080"); err != nil {
@@ -15,4 +16,9 @@ func main() {
 	} else {
 		fmt.Println("Server started on port 8080")
 	}
+}
+func main() {
+	initWeb()
+	a := app.New()
+	a.Icon()
 }
